e90aacb4f39af058c27a0505a0167b953335006a
DCIAL42 <daniel05cooke@gmail.com>
2025-09-22 17:13:08 +0100

    added typst preview plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -136,6 +136,13 @@ return {
         }
     },
 
+    {
+        'chomosuke/typst-preview.nvim',
+        lazy = false, -- or ft = 'typst'
+        version = '1.*',
+        opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+    },
+
     --use("eandrju/cellular-automaton.nvim")
     --use("lervag/vimtex")
     --use {
