e16145a8f4ca2a78e4a1eb5276cfab09da8881e2
DCIAL42 <daniel05cooke@gmail.com>
2025-10-06 00:06:18 +0100

    changed make remap to save file

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,6 +1,6 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("n", "<leader>r", ":make<CR>")
+vim.keymap.set("n", "<leader>r", ":w<CR>:make<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
