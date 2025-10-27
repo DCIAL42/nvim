2b4e44df65aea13f9bf61019401e92a46ab8071b
DCIAL42 <daniel05cooke@gmail.com>
2025-10-27 21:02:04 +0000

    plugin stuff

--- lua/plugin/testfloat.lua
+++ lua/plugin/testfloat.lua
@@ -5,11 +5,9 @@ local state = {
         id = 0,
         extmarks = {},
     },
-    files = {},
 }
 
---- Takes output from git blame --line-porcelain
---- for the current file and parses them
+--- Takes output from git blame for the current file and parses them
 ---@param data string[]
 ---@return table
 local parse_blames = function(data)
@@ -36,7 +34,6 @@ end
 
 M.load_blames = function()
     local file = vim.fn.expand("%")
-    vim.print(file)
     if file == "" then return end
 
     local stdout_chunks = {}
@@ -44,7 +41,7 @@ M.load_blames = function()
     local job = vim.fn.jobstart(
         { "git", "blame", file, "--line-porcelain" },
         {
-            on_stdout = function(_, data, _)
+            on_stdout = function(_, data)
                 if data then
                     for _, line in ipairs(data) do
                         table.insert(stdout_chunks, line)
@@ -52,25 +49,25 @@ M.load_blames = function()
                 end
             end,
 
-            on_exit = function(_, exit_code, _)
+            on_exit = function(_, exit_code)
                 if exit_code == 0 then
-                    local stuff = parse_blames(stdout_chunks)
-                    state.files[file] = stuff
-                    M.show_git_blame()
+                    M.show_git_blame(parse_blames(stdout_chunks))
                 end
             end,
         }
     )
 end
 
-M.show_git_blame = function()
-    local file_data = state.files[vim.fn.expand("%")]
+local parse_time = function(time)
+    local time_table = os.date("*t", os.time() - time)
+    vim.print(time_table)
+end
 
-    vim.print(file_data)
-    if not file_data then return end
+M.show_git_blame = function(data)
+    if not data then return end
 
     local prev = ""
-    for k, line in ipairs(file_data) do
+    for k, line in ipairs(data) do
         local commit = line.commit
 
         local start = string.find(commit, " ") + 1
@@ -83,20 +80,24 @@ M.show_git_blame = function()
         local row = tonumber(string.sub(commit, mid, endpos))
 
         local blame_text = author .. " • " .. message
-        local time = tonumber(line["committer-time"])
-        print(os.date("%M", os.time() - time))
 
         if hash == "0000000000000000000000000000000000000000" then
             blame_text = "Uncommitted"
-            vim.fn.sign_place(0, 'my_sign_group', 'UncommittedSign', vim.api.nvim_buf_get_name(0), { lnum = k })
+            vim.fn.sign_place(0, 'uncommitted_sign_group', 'UncommittedSign', vim.api.nvim_buf_get_name(0), { lnum = k })
         end
 
         if row ~= nil and hash ~= prev then
+            local time = tonumber(line["committer-time"])
+
+            parse_time(time)
+
             vim.api.nvim_buf_set_extmark(0, state.namespace.id, row - 1, 0, {
                 virt_text = { { blame_text, "Comment" } },
                 virt_text_pos = "eol",
             })
+            -- state.namespace.extmarks:insert(m)
         end
+
         prev = hash
     end
 end
